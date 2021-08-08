2e8ad70889cc57a71dcf7a88f3d25bd3f0c90574
Your Name <you@example.com>
2021-08-08 04:17:31 -0300

    adiciona datas

--- ClinicaTelemedicina/ClinicaTelemedicina/ClinicaTelemedicina/src/main/java/org/example/CalendarioController.java
+++ ClinicaTelemedicina/ClinicaTelemedicina/ClinicaTelemedicina/src/main/java/org/example/CalendarioController.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
@@ -14,6 +19,8 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class CalendarioController implements Initializable {
+
+	LocalDate day = LocalDate.now();
 
 	public void logout() {
 		try {
@@ -35,6 +42,17 @@ public class CalendarioController implements Initializable {
 		App.setRoot("CadastroPaciente");
 	}
 
+	public void previousWeek(ActionEvent actionEvent) {
+		day = day.minusDays(7);
+		init();
+	}
+
+	public void nextWeek(ActionEvent actionEvent) {
+		day = day.plusDays(7);
+		System.out.println(day);
+		init();
+	}
+
 	public class Hora {
 		private final String hora;
 
@@ -47,26 +65,19 @@ public class CalendarioController implements Initializable {
 		}
 	}
 
-	@FXML
-	private TableView<Hora> Calendar;
+	@FXML private TableView<Hora> calendar;
 
-	@FXML
-	private TableColumn<Hora, String> hora;
+	@FXML private TableColumn<Hora, String> hora;
 
-	@FXML
-	private TableColumn<Hora, String> MONDAY;
+	@FXML private TableColumn<Hora, String> MONDAY;
 
-	@FXML
-	private TableColumn<Hora, String> TUESDAY;
+	@FXML private TableColumn<Hora, String> TUESDAY;
 
-	@FXML
-	private TableColumn<Hora, String> WEDNESDAY;
+	@FXML private TableColumn<Hora, String> WEDNESDAY;
 
-	@FXML
-	private TableColumn<Hora, String> THURSDAY;
+	@FXML private TableColumn<Hora, String> THURSDAY;
 
-	@FXML
-	private TableColumn<Hora, String> FRIDAY;
+	@FXML private TableColumn<Hora, String> FRIDAY;
 
 	ObservableList<Hora> list = FXCollections.observableArrayList(
 			new Hora("08:00"),
@@ -81,20 +92,35 @@ public class CalendarioController implements Initializable {
 			new Hora("17:00")
 	);
 
+	public void init(){
+		hora.setCellValueFactory(new PropertyValueFactory<Hora, String>("hora"));
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+		MONDAY.setText("SEGUNDA\n" +
+				day.with(DayOfWeek.MONDAY).format(formatter));
+		TUESDAY.setText("TERÇA\n" +
+				day.with(DayOfWeek.TUESDAY).format(formatter));
+		WEDNESDAY.setText("QUARTA\n" +
+				day.with(DayOfWeek.WEDNESDAY).format(formatter));
+		THURSDAY.setText("QUINTA\n" +
+				day.with(DayOfWeek.THURSDAY).format(formatter));
+		FRIDAY.setText("SEXTA\n" +
+				day.with(DayOfWeek.FRIDAY).format(formatter));
+
+		calendar.setItems(list);
+	}
+
 	@Override
 	public void initialize(URL url, ResourceBundle resourceBundle) {
-		hora.setCellValueFactory(new PropertyValueFactory<Hora, String>("hora"));
-		MONDAY.setText("É sett\next sim");
-
-		Calendar.setItems(list);
+		init();
 	}
 
 	@FXML
 	public void clickItem(MouseEvent event) {
 		if(event.getClickCount() == 2) {
-			System.out.print(Calendar.getSelectionModel().getSelectedItem().getHora());
+			System.out.print(calendar.getSelectionModel().getSelectedItem().getHora());
 			System.out.print(" ");
-			System.out.println((Calendar.getFocusModel().getFocusedCell().getTableColumn().getId()));
+			System.out.println((calendar.getFocusModel().getFocusedCell().getTableColumn().getId()));
 		}
 	}
 }
