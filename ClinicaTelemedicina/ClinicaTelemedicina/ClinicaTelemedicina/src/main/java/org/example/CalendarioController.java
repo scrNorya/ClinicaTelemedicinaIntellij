e56bd427596c8917fa68cae2a0d8466eb149396f
Caio Noguerol <caionoguerol@gmail.com>
2021-08-08 01:07:43 -0300

    Ajuste calendario
    Ajuste calendario

--- ClinicaTelemedicina/ClinicaTelemedicina/ClinicaTelemedicina/src/main/java/org/example/CalendarioController.java
+++ ClinicaTelemedicina/ClinicaTelemedicina/ClinicaTelemedicina/src/main/java/org/example/CalendarioController.java
@@ -1,13 +1,16 @@
 package org.example;
 
 import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
+import org.example.utils.View;
 
 import java.io.IOException;
 import java.net.URL;
@@ -15,86 +18,106 @@ import java.util.ResourceBundle;
 
 public class CalendarioController implements Initializable {
 
-	public void logout() {
-		try {
-			App.setRoot("Login");
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-	}
-	
-	public void createRecepcionista() throws IOException {
-		App.setRoot("CadastroRecepcionista");
-	}
+    public void logout() {
+        try {
+            App.setRoot("Login");
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+    }
 
-	public void createMedico() throws IOException {
-		App.setRoot("CadastroMedico");
-	}
+    public void createRecepcionista() throws IOException {
+        App.setRoot("CadastroRecepcionista");
+    }
 
-	public void createPaciente() throws IOException {
-		App.setRoot("CadastroPaciente");
-	}
+    public void createMedico() throws IOException {
+        App.setRoot("CadastroMedico");
+    }
 
-	public class Hora {
-		private final String hora;
+    public void createPaciente() throws IOException {
+        App.setRoot("CadastroPaciente");
+    }
 
-		public Hora(String hora) {
-			this.hora = hora;
-		}
+    public class Hora {
+        private final String hora;
 
-		public String getHora() {
-			return hora;
-		}
-	}
+        public Hora(String hora) {
+            this.hora = hora;
+        }
 
-	@FXML
-	private TableView<Hora> Calendar;
+        public String getHora() {
+            return hora;
+        }
+    }
 
-	@FXML
-	private TableColumn<Hora, String> hora;
+    @FXML
+    private TableView<Hora> Calendar;
+    @FXML
+    private TableColumn<Hora, String> hora;
 
-	@FXML
-	private TableColumn<Hora, String> MONDAY;
+    @FXML
+    private TableColumn<Hora, String> MONDAY;
 
-	@FXML
-	private TableColumn<Hora, String> TUESDAY;
+    @FXML
+    private TableColumn<Hora, String> TUESDAY;
 
-	@FXML
-	private TableColumn<Hora, String> WEDNESDAY;
+    @FXML
+    private TableColumn<Hora, String> WEDNESDAY;
 
-	@FXML
-	private TableColumn<Hora, String> THURSDAY;
+    @FXML
+    private TableColumn<Hora, String> THURSDAY;
 
-	@FXML
-	private TableColumn<Hora, String> FRIDAY;
+    @FXML
+    private TableColumn<Hora, String> FRIDAY;
 
-	ObservableList<Hora> list = FXCollections.observableArrayList(
-			new Hora("08:00"),
-			new Hora("09:00"),
-			new Hora("10:00"),
-			new Hora("11:00"),
-			new Hora("12:00"),
-			new Hora("13:00"),
-			new Hora("14:00"),
-			new Hora("15:00"),
-			new Hora("16:00"),
-			new Hora("17:00")
-	);
+    @FXML
+    private ComboBox<String> medicoComboBox;
 
-	@Override
-	public void initialize(URL url, ResourceBundle resourceBundle) {
-		hora.setCellValueFactory(new PropertyValueFactory<Hora, String>("hora"));
-		MONDAY.setText("É sett\next sim");
 
-		Calendar.setItems(list);
-	}
+    ObservableList<Hora> list = FXCollections.observableArrayList(
+            new Hora("08:00"),
+            new Hora("09:00"),
+            new Hora("10:00"),
+            new Hora("11:00"),
+            new Hora("12:00"),
+            new Hora("13:00"),
+            new Hora("14:00"),
+            new Hora("15:00"),
+            new Hora("16:00"),
+            new Hora("17:00")
+    );
+    // <<<<<<<<<<<PEGAR DADOS DO JSON PARA A MEDICOSLIST!!!!!!>>>>>>>>>>>>
+    ObservableList<String> medicosList = FXCollections.observableArrayList("Teste", "teste 1", "teste 2", "teste3");
 
-	@FXML
-	public void clickItem(MouseEvent event) {
-		if(event.getClickCount() == 2) {
-			System.out.print(Calendar.getSelectionModel().getSelectedItem().getHora());
-			System.out.print(" ");
-			System.out.println((Calendar.getFocusModel().getFocusedCell().getTableColumn().getId()));
-		}
-	}
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        hora.setCellValueFactory(new PropertyValueFactory<Hora, String>("hora"));
+        medicoComboBox.setItems(medicosList);
+        Calendar.setItems(list);
+        Calendar.getSelectionModel().setCellSelectionEnabled(true);
+        Calendar.getColumns().addListener(new ListChangeListener() {
+            @Override
+            public void onChanged(Change change) {
+                change.next();
+                if (change.wasReplaced()) {
+                    Calendar.getColumns().clear();
+                    Calendar.getColumns().addAll(hora, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY);
+                }
+            }
+        });
+    }
+
+    @FXML
+    public void clickItem(MouseEvent event) {
+        String id = Calendar.getFocusModel().getFocusedCell().getTableColumn().getId();
+        if (medicoComboBox.getValue()!=null) {
+            if (event.getClickCount() == 2 && !id.equals("hora")) {
+                System.out.print(Calendar.getSelectionModel().getSelectedItem().getHora());
+                System.out.print(" ");
+                System.out.println((Calendar.getFocusModel().getFocusedCell().getTableColumn().getId()));
+            }
+        }else{
+            View.generateAlert("Selecione um médico!");
+        }
+    }
 }
