5366798772b59035f5b2a778c0008ccaa898678a
Your Name <you@example.com>
2021-09-21 22:09:13 -0300

    não informado pra quando não tem cid e agora segue RN (ou requisito, n lembro)

--- ClinicaTelemedicina/ClinicaTelemedicina/ClinicaTelemedicina/src/main/java/org/example/HistoricoMedicoController.java
+++ ClinicaTelemedicina/ClinicaTelemedicina/ClinicaTelemedicina/src/main/java/org/example/HistoricoMedicoController.java
@@ -12,6 +12,7 @@ import org.example.utils.ViewUtils;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
@@ -34,22 +35,37 @@ public class HistoricoMedicoController {
             String consultasText = "";
             String dataFormatada;
             String[] splittedDate;
+            boolean temConsultaAgora = false;
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
             for (Consulta c : historico) {
                 splittedDate = c.getData().split("-");
-                LocalDate dataHora = LocalDate.of(
+                LocalDateTime dataHora = LocalDateTime.of(
                         Integer.parseInt(splittedDate[0]),
                         Integer.parseInt(splittedDate[1]),
-                        Integer.parseInt(splittedDate[2])
+                        Integer.parseInt(splittedDate[2]),
+                        Integer.parseInt(c.getHorario()),
+                        0
                 );
-                dataFormatada = dataHora.format(formatter);
-                consultasText +=
-                    dataFormatada + " - " + c.getHorario() + ":00h\n" +
-                            "    Sala: " + c.getSala() + "\n" +
-                            "    CID: " + c.getCid() + "\n" +
-                            "    Diagnostico: " + c.getDiagnostico() + "\n\n";
-        }
-            consultas.setText(consultasText);
+                if (dataHora.getHour() == LocalDateTime.now().getHour()) {
+                    temConsultaAgora = true;
+                }
+                    dataFormatada = dataHora.format(formatter);
+                    consultasText +=
+                            dataFormatada + " - " + c.getHorario() + ":00h\n" +
+                                    "    Sala: " + c.getSala() + "\n" +
+                                    "    CID: " + (c.getCid().equals("") ? "Não informado." : c.getCid()) + "\n" +
+                                    "    Diagnostico: " + c.getDiagnostico() + "\n\n";
+            }
+            if (temConsultaAgora) {
+                consultas.setText(consultasText);
+            } else {
+                consultas.setText("");
+                ViewUtils.generateAlert("Paciente sem consulta nesse horário " +
+                        "(" + LocalDateTime.now().getHour() + ":" + LocalDateTime.now().getMinute() + ").\n Para visualizar " +
+                        "o histórico médico é necessário que o paceinte tenha uma consulta " +
+                        "marcada para esse horário com você.");
+
+            }
         } catch (Exception e) {
             consultas.setText("");
             ViewUtils.generateAlert(e.getMessage());
